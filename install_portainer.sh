feb507b433039954e400546026004e70f9b43ba7
Vladislav <svetovoyvv@yandex.ru>
2025-10-18 18:12:32 +0300

    add portainer

--- install_portainer.sh
+++ install_portainer.sh
@@ -1,4 +1,6 @@
-apt install apache2-utils
+apt install -y apache2-utils
 PASSWORD="$(cat /etc/services-password.txt)"
 PASSWORD_HASH="$(htpasswd -nb -B "admin" "$PASSWORD")"
-docker compose -f portainer-compose.yaml up -d
+echo "Сгенерированный хеш пароля: $PASSWORD_HASH"
+echo "PASSWORD_HASH=$PASSWORD_HASH" > .portainer.env
+docker compose -f portainer-compose.yaml --env-file .portainer.env up -d
