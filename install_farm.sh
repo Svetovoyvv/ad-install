edf70a65bfc5d6db1e12a056fc32e4fb8ba717d9
Vladislav <svetovoyvv@yandex.ru>
2025-10-18 18:30:33 +0300

    configure ports

--- install_farm.sh
+++ install_farm.sh
@@ -5,7 +5,7 @@ fi
 git clone https://github.com/Svetovoyvv/UfoFarm.git "$INSTALL_PATH"
 cd "$INSTALL_PATH"
 FARM_SERVER_PASSWORD="$(cat /etc/services-password.txt)"
-echo "Generated password: $FARM_SERVER_PASSWORD"
 export FARM_SERVER_PASSWORD="$FARM_SERVER_PASSWORD"
+export FARM_PORT="47503"
 docker compose up --build -d
 cd -
--- install_firegex.sh
+++ install_firegex.sh
@@ -1,5 +1,6 @@
 mkdir firegex
 cd firegex
 sh <(curl -sLf https://pwnzer0tt1.it/firegex.sh)
-python3 firegex.py start --standalone
+PASSWORD="$(cat /etc/services-password.txt)"
+python3 firegex.py start --standalone --port 47502 --psw-on-web --startup-psw "$PASSWORD"
 cd -
