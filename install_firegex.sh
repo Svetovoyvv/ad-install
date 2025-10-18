ab0d5ab91fc2ca9ad283a44e00658edb462435e4
Vladislav <svetovoyvv@yandex.ru>
2025-10-18 18:44:59 +0300

    install firegex

--- install_firegex.sh
+++ install_firegex.sh
@@ -1,6 +1,5 @@
 mkdir firegex
 cd firegex
-sh <(curl -sLf https://pwnzer0tt1.it/firegex.sh)
 PASSWORD="$(cat /etc/services-password.txt)"
-python3 firegex.py start --standalone --port 47502 --psw-on-web --startup-psw "$PASSWORD"
+sh <(curl -sLf https://pwnzer0tt1.it/firegex.sh) start --standalone --port 47502 --psw-on-web --startup-psw "$PASSWORD"
 cd -
